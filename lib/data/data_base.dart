a071123e98e4f99c1c53cd9cdbf93a3e15128e14
Sarafaraj Nasardi <75375728+sarafarajnasardi@users.noreply.github.com>
2024-12-29 08:43:10 +0530

    Update data_base.dart

--- lib/data/data_base.dart
+++ lib/data/data_base.dart
@@ -5,12 +5,10 @@ import 'package:hive/hive.dart';
 class ToDoDatabase {
   List toDoList = [];
 
-  // Load data from Hive box into `toDoList`
   void loadData() {
     toDoList = Hive.box('mybox').get("TODOLIST", defaultValue: []);
   }
 
-  // Update Hive box with the current `toDoList`
   void updateDataBase() {
     Hive.box('mybox').put("TODOLIST", toDoList);
   }
